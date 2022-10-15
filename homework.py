6fdb12e67fe14682a3b58011c559516303758e98
Alex Romantsov <alekseyromantsov@gmail.com>
2022-10-15 21:37:10 +0300

    before

--- homework.py
+++ homework.py
@@ -69,12 +69,12 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    CALORIES_MEAN_SPEED_MULTIPLIER : int = 18
+    CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
     CALORIES_MEAN_SPEED_SHIFT: int = 1.79
 
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER * self.get_mean_speed()
-                 - self.CALORIES_MEAN_SPEED_SHIFT )
+                 - self.CALORIES_MEAN_SPEED_SHIFT)
                 * self.weight
                 / self.M_IN_KM
                 * self.duration * self.MIN_IN_H)
